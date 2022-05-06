d3ff60689f449eccda6298f89083057908e5f4f3
seryl lounis <seryllounis@outlook.fr>
2022-05-06 13:51:25 +0200

    create category component and directory component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Directory from "./components/directory/directory.component";
 
-function App() {
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Chapeaux",
+      imageUrl:
+        "https://p0.piqsels.com/preview/995/174/448/women-fashion-boho-hat.jpg",
+    },
+    {
+      id: 2,
+      title: "Vestes",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "Baskets",
+      imageUrl:
+        "https://p0.piqsels.com/preview/936/972/1/fashionsportvarious-clothes-feet-footwear.jpg",
+    },
+    {
+      id: 4,
+      title: "Femmes",
+      imageUrl: "https://p2.piqsels.com/preview/656/541/86/596c933d41129.jpg",
+    },
+    {
+      id: 5,
+      title: "Hommes",
+      imageUrl: "https://p0.piqsels.com/preview/190/704/992/5be94621dbf86.jpg",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Directory categories={categories}/>
   );
-}
+};
 
 export default App;

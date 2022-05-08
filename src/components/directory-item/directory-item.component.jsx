576fad7b6ccf296ba0e9eb6966321a6dd6847c44
seryl lounis <seryllounis@outlook.fr>
2022-05-08 18:30:04 +0200

    add some details for navigate

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import { BackgroundImage, DirectoryBodyContainer, DirectoryItemContainer } from "./directory-item.styles";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  BackgroundImage,
+  DirectoryBodyContainer,
+  DirectoryItemContainer,
+} from "./directory-item.styles";
 
-const DirectoryItem = ({category}) => {
-  const {imageUrl, title} = category
+const DirectoryItem = ({ category }) => {
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+  const handleNavigate = () => navigate(route);
+
   return (
-    <DirectoryItemContainer>
-      <BackgroundImage
-        imageUrl={imageUrl}
-      />
-      <DirectoryBodyContainer to={`/shop/${title.toLowerCase()}`} >
+    <DirectoryItemContainer onClick={handleNavigate}>
+      <BackgroundImage imageUrl={imageUrl} />
+      <DirectoryBodyContainer>
         <h2>{title}</h2>
         <p>Tout voir</p>
       </DirectoryBodyContainer>
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,41 @@
 import DirectoryItem from "../directory-item/directory-item.component";
 import { DirectoryContainer } from "./directory.style";
 
-const Directory = ({ categories }) => {
+const categories = [
+  {
+    id: 1,
+    title: "Chapeaux",
+    imageUrl:
+      "https://p0.piqsels.com/preview/995/174/448/women-fashion-boho-hat.jpg",
+    route: "shop/chapeaux",
+  },
+  {
+    id: 2,
+    title: "Vestes",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    route: "shop/vestes",
+  },
+  {
+    id: 3,
+    title: "Baskets",
+    imageUrl:
+      "https://p0.piqsels.com/preview/936/972/1/fashionsportvarious-clothes-feet-footwear.jpg",
+    route: "shop/baskets",
+  },
+  {
+    id: 4,
+    title: "Femmes",
+    imageUrl: "https://p2.piqsels.com/preview/656/541/86/596c933d41129.jpg",
+    route: "shop/femmes",
+  },
+  {
+    id: 5,
+    title: "Hommes",
+    imageUrl: "https://p0.piqsels.com/preview/190/704/992/5be94621dbf86.jpg",
+    route: "shop/hommes",
+  },
+];
+const Directory = () => {
   return (
     <DirectoryContainer>
       {categories.map((category) => {
